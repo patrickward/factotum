3b91687cbe45a00d49908cf07d6439937f1646f3
Patrick Ward <patrick@octetic.com>
2024-12-11 05:36:09 -0500

    correction to logger

--- logger.go
+++ logger.go
@@ -3,70 +3,85 @@ package faktotum
 import (
 	"fmt"
 	"log/slog"
+	"strings"
 )
 
 // FaktoryLogger is a logger that implements the Logger interface from the faktory_worker package.
-// Its wraps the slog.Logger type from the slog package.
+// It wraps the slog.Logger type from the slog package.
 type FaktoryLogger struct {
 	logger *slog.Logger
+	prefix string
 }
 
 // NewFaktoryLogger creates a new FaktoryLogger.
 func NewFaktoryLogger(logger *slog.Logger) *FaktoryLogger {
-	return &FaktoryLogger{logger: logger}
+	return &FaktoryLogger{
+		logger: logger,
+		prefix: "[Faktory] ",
+	}
+}
+
+// convertArgsToMessage converts variadic arguments to a string message
+func (l *FaktoryLogger) convertArgsToMessage(v ...interface{}) string {
+	// Convert all arguments to strings and join them
+	parts := make([]string, len(v))
+	for i, arg := range v {
+		parts[i] = fmt.Sprint(arg)
+	}
+	return l.prefix + strings.Join(parts, " ")
 }
 
 // Debug logs a message at the debug level.
 func (l *FaktoryLogger) Debug(v ...interface{}) {
-	l.logger.Debug("Faktory Debug", v...)
+	msg := l.convertArgsToMessage(v...)
+	l.logger.Debug(msg)
 }
 
 // Debugf logs a message at the debug level with a format string.
-func (l *FaktoryLogger) Debugf(format string, v ...interface{}) {
-	message := fmt.Sprintf(format, v...)
-	l.logger.Debug(message)
+func (l *FaktoryLogger) Debugf(format string, args ...interface{}) {
+	l.logger.Debug(l.prefix + fmt.Sprintf(format, args...))
 }
 
 // Info logs a message at the info level.
 func (l *FaktoryLogger) Info(v ...interface{}) {
-	l.logger.Info("Faktory Info", v...)
+	msg := l.convertArgsToMessage(v...)
+	l.logger.Info(msg)
 }
 
 // Infof logs a message at the info level with a format string.
-func (l *FaktoryLogger) Infof(format string, v ...interface{}) {
-	message := fmt.Sprintf(format, v...)
-	l.logger.Info(message)
+func (l *FaktoryLogger) Infof(format string, args ...interface{}) {
+	l.logger.Info(l.prefix + fmt.Sprintf(format, args...))
 }
 
 // Warn logs a message at the warn level.
 func (l *FaktoryLogger) Warn(v ...interface{}) {
-	l.logger.Warn("Faktory Warn", v...)
+	msg := l.convertArgsToMessage(v...)
+	l.logger.Warn(msg)
 }
 
 // Warnf logs a message at the warn level with a format string.
-func (l *FaktoryLogger) Warnf(format string, v ...interface{}) {
-	message := fmt.Sprintf(format, v...)
-	l.logger.Warn(message)
+func (l *FaktoryLogger) Warnf(format string, args ...interface{}) {
+	l.logger.Warn(l.prefix + fmt.Sprintf(format, args...))
 }
 
 // Error logs a message at the error level.
 func (l *FaktoryLogger) Error(v ...interface{}) {
-	l.logger.Error("Faktory Error", v...)
+	msg := l.convertArgsToMessage(v...)
+	l.logger.Error(msg)
 }
 
 // Errorf logs a message at the error level with a format string.
-func (l *FaktoryLogger) Errorf(format string, v ...interface{}) {
-	message := fmt.Sprintf(format, v...)
-	l.logger.Error(message)
+func (l *FaktoryLogger) Errorf(format string, args ...interface{}) {
+	l.logger.Error(l.prefix + fmt.Sprintf(format, args...))
 }
 
-// Fatal logs a message at the fatal level.
+// Fatal logs a message at the fatal level and exits the program.
 func (l *FaktoryLogger) Fatal(v ...interface{}) {
-	l.logger.Error("Faktory Fatal", v...)
+	msg := l.convertArgsToMessage(v...)
+	l.logger.Error(msg, slog.String("level", "fatal"))
 }
 
-// Fatalf logs a message at the fatal level with a format string.
-func (l *FaktoryLogger) Fatalf(format string, v ...interface{}) {
-	message := fmt.Sprintf(format, v...)
-	l.logger.Error(message)
+// Fatalf logs a message at the fatal level with a format string and exits the program.
+func (l *FaktoryLogger) Fatalf(format string, args ...interface{}) {
+	l.logger.Error(l.prefix+fmt.Sprintf(format, args...), slog.String("level", "fatal"))
 }
